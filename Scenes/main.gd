547e2ae7278bb6d8df0ec8598b2a506ef3cb042f
Arreb <tassnass2018@gmail.com>
2026-09-03 11:07:27 +0200

    började

---
+++ Scenes/main.gd
@@ -0,0 +1,7 @@
+extends Node2D
+
+@onready var world_label = $WordLabel
+@onready var guess_input = $GuessInput
+@onready var guess_button = $GuessButton
+@onready var feedback_label = $FeedbackLabel
+@onready var tries_left_label = $TriesLeftlabel
