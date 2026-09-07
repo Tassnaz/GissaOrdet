e762f0a80e01147becc7bbe73132dc0cc472f015
Arreb <tassnass2018@gmail.com>
2026-09-07 14:16:23 +0200

    Väljer random ord från lista och man kan gissa

--- Scenes/main.gd
+++ Scenes/main.gd
@@ -1,7 +1,31 @@
 extends Node2D
 
-@onready var world_label = $WordLabel
-@onready var guess_input = $GuessInput
-@onready var guess_button = $GuessButton
-@onready var feedback_label = $FeedbackLabel
-@onready var tries_left_label = $TriesLeftlabel
+var word_list: Array = ["Guccikeps", "Ejnar", "Allardismen"]
+
+var current_word: String = ""
+var tries_left: int = 5
+
+@onready var world_label: Label = $WordLabel
+@onready var guess_input: LineEdit = $GuessInput
+@onready var guess_button: Button = $GuessButton
+@onready var feedback_label: Label = $FeedbackLabel
+@onready var tries_left_label: Label = $TriesLeftlabel
+
+func _ready() -> void:
+	new_word()
+	world_label.text = current_word
+
+func new_word():
+	current_word = word_list.pick_random()
+	
+	
+func _on_guess_button_pressed() -> void:
+	if guess_input.text == current_word:
+		feedback_label.text = "Correct"
+		new_word()
+		guess_input.clear()
+		world_label.text = current_word
+		
+	else:
+		feedback_label.text = "Wrong"
+		guess_input.clear()
